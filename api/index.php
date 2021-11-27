996eafcc162e4ae4859c3def219036cd13f9dfe3
Lukáš Koliandr <koliandr.lu@gmail.com>
2021-11-27 20:29:42 +0100

    Fix API vs api soubory

---
+++ api/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use Kantodo\Auth\Auth;
+use Kantodo\API\API;
+use Kantodo\API\Controllers\AuthController;
+use Kantodo\API\Controllers\ProjectController;
+use Kantodo\API\Controllers\TaskController;
+use Kantodo\API\Response;
+
+use function Kantodo\Core\Functions\t;
+
+include "../Loader/autoload.php";
+
+
+$API = new API();
+$API->registerAuth(new Auth());
+
+// TODO: odstranit
+API::debugMode(true);
+
+$session = $API->session;
+$session->start();
+
+$API->router->registerErrorCodeHandler(API::ERROR_NOT_AUTHORIZED, function () {
+    API::$API->response->error(t('not_authorized'));
+});
+
+$API->router->post('create/project', [ProjectController::class, 'create'], API::USER);
+$API->router->post('create/task', [TaskController::class, 'create'], API::USER);
+$API->router->get('get/task/{projectUUID}', [TaskController::class, 'get'], API::USER);
+$API->router->post('auth/refreshToken', [AuthController::class, 'refreshToken'], API::USER);
+
+$API->run();
+
+?>
